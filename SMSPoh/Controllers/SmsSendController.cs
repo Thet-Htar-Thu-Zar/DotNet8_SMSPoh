7dea3a5c4675cceeffa9d6b0e548007de804cf00
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-02-20 09:41:38 +0630

    using sms poh account

--- SMSPoh/Controllers/SmsSendController.cs
+++ SMSPoh/Controllers/SmsSendController.cs
@@ -5,6 +5,8 @@ using System.Net.Http.Headers;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
 using System.Net;
+using System.Buffers.Text;
+using Microsoft.AspNetCore.Authentication;
 
 namespace SMSPoh.Controllers
 {
@@ -26,16 +28,28 @@ namespace SMSPoh.Controllers
         {
             try
             {
-                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _configuration.GetSection("AuthKey").Value!);
+                string apiKey = _configuration.GetSection("ApiKey").Value;
+                string apiSecret = _configuration.GetSection("ApiSecret").Value;
+
+                // Concatenate the API Key and API Secret with a colon
+                string concatenated = $"{apiKey}:{apiSecret}";
+
+                // Convert the concatenated string to a byte array
+                byte[] bytesToEncode = Encoding.UTF8.GetBytes(concatenated);
+
+                // Encode the byte array to a Base64 string
+                string base64Encoded = Convert.ToBase64String(bytesToEncode);
+
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", base64Encoded);
                 string jsonStr = JsonConvert.SerializeObject(Sms);
                 HttpContent content = new StringContent(jsonStr, Encoding.UTF8, Application.Json);
-                HttpResponseMessage response = await _httpClient.PostAsync("/send", content);
+                HttpResponseMessage response = await _httpClient.PostAsync("https://v3.smspoh.com/api/rest/send", content);
                 //HttpResponseMessage response = await _httpClient.PostAsync("/api/send", content);
 
                 string message = await response.Content.ReadAsStringAsync();
                 if (response.IsSuccessStatusCode)
                 {
-                    return Ok(message);
+                    return Ok(JsonConvert.DeserializeObject(message));
                 }
                 return BadRequest(message);
             }
